b8fc00d8ec160153f9c0b1f337f2ca46fa5976d1
Sandro Knauß <mail@sandroknauss.de>
2015-08-28 14:34:15 +0200

    Extract strings from ui files too

--- Messages.sh
+++ Messages.sh
@@ -1,2 +1,4 @@
 #! /usr/bin/env bash
-$XGETTEXT *.cpp -o $podir/akonadi_ldap_resource.pot
+$EXTRACTRC `find . -name \*.ui` >> rc.cpp
+$XGETTEXT `find . -name \*.h -o -name \*.cpp | grep -v '/tests/'` -o $podir/akonadi_ldap_resource.pot
+rm -f rc.cpp
